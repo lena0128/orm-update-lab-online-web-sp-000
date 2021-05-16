9f26e26be017a080fc819682764f686ed1277a61
Lena Hou <lenahou2013@gmail.com>
2021-05-16 03:17:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,10 +54,10 @@ class Student
  end
 end
 
-def self.create(name:, grade:)
-    s = Student.new(name, grade)
-    s.save
-    s
+def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 
 def self.find_by_name(name)
