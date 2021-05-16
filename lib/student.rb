50ff4751d22cb7e4b26b4ae07b9174d5b4acb44f
Lena Hou <lenahou2013@gmail.com>
2021-05-16 03:01:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,4 +31,16 @@ class Student
 
     DB[:conn].execute(sql)
   end
+
+ def save
+  sql = <<-SQL
+    INSERT INTO students(name, grade)
+    VALUES (?, ?)
+  SQL
+
+  DB[:conn].execute(sql, self.name, self.grade)
+  @id = DB[:conn].execute("SELECT last_insertrowid() FROM students")[0][0]
+ end
+
+
 end
