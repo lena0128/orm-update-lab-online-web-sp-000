b1b390aec786287d1679d312d02a0e2b30377782
Lena Hou <lenahou2013@gmail.com>
2021-05-16 03:22:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
      if self.id
        self.update
      else
-       sql = <<-SQL 
+       sql = <<-SQL
          INSERT INTO students (name, grade)
          VALUES (?,?)
        SQL
